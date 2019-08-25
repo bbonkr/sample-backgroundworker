9d37a95bb9171c817c70db84b8ef5b4100e323c7
Pon Cheol Ku <bbon@bbon.kr>
2019-08-25 16:03:17 +0900

    ADDED: - add vs project

---
+++ SampleBackGroundWorker/Form1.Designer.cs
@@ -0,0 +1,150 @@
+﻿namespace SampleBackGroundWorker
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다.
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마십시오.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.bgWorker = new System.ComponentModel.BackgroundWorker();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.progressBar = new System.Windows.Forms.ProgressBar();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.lblWorkCount = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btnError = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(19, 20);
+            this.btnStart.Margin = new System.Windows.Forms.Padding(4);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(107, 34);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "시작";
+            this.btnStart.UseVisualStyleBackColor = true;
+            // 
+            // progressBar
+            // 
+            this.progressBar.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.progressBar.Location = new System.Drawing.Point(0, 236);
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(585, 26);
+            this.progressBar.Step = 1;
+            this.progressBar.Style = System.Windows.Forms.ProgressBarStyle.Continuous;
+            this.progressBar.TabIndex = 1;
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Location = new System.Drawing.Point(134, 20);
+            this.btnCancel.Margin = new System.Windows.Forms.Padding(4);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(107, 34);
+            this.btnCancel.TabIndex = 0;
+            this.btnCancel.Text = "취소";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            // 
+            // lblWorkCount
+            // 
+            this.lblWorkCount.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblWorkCount.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.lblWorkCount.Location = new System.Drawing.Point(134, 58);
+            this.lblWorkCount.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblWorkCount.Name = "lblWorkCount";
+            this.lblWorkCount.Size = new System.Drawing.Size(438, 34);
+            this.lblWorkCount.TabIndex = 2;
+            this.lblWorkCount.Text = "label1";
+            this.lblWorkCount.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // label1
+            // 
+            this.label1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.label1.Location = new System.Drawing.Point(17, 58);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(109, 34);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "작업건수 : ";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.Location = new System.Drawing.Point(12, 104);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(561, 126);
+            this.textBox1.TabIndex = 3;
+            // 
+            // btnError
+            // 
+            this.btnError.Location = new System.Drawing.Point(249, 20);
+            this.btnError.Margin = new System.Windows.Forms.Padding(4);
+            this.btnError.Name = "btnError";
+            this.btnError.Size = new System.Drawing.Size(107, 34);
+            this.btnError.TabIndex = 4;
+            this.btnError.Text = "예외";
+            this.btnError.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(585, 262);
+            this.Controls.Add(this.btnError);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblWorkCount);
+            this.Controls.Add(this.progressBar);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnStart);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "비동기실행데모";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.ComponentModel.BackgroundWorker bgWorker;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.ProgressBar progressBar;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.Label lblWorkCount;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btnError;
+    }
+}
+
---
+++ SampleBackGroundWorker/Form1.cs
@@ -0,0 +1,237 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SampleBackGroundWorker
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            this.Load += Form1_Load;
+            this.btnStart.Click += Click_btnStart;
+            this.btnCancel.Click += Click_btnCancel;
+            this.btnError.Click += Click_btnError;
+
+            // BackgroundWorker 
+            this.bgWorker.DoWork += bgWorker_DoWork;
+            this.bgWorker.ProgressChanged += bgWorker_ProgressChanged;
+            this.bgWorker.RunWorkerCompleted += bgWorker_RunWorkerCompleted;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                // 작업 취소를 사용
+                this.bgWorker.WorkerSupportsCancellation = true;
+
+                // 작업 진행률 업데이트 보고를 사용
+                this.bgWorker.WorkerReportsProgress = true;
+                this.btnStart.Enabled = true;
+                this.btnCancel.Enabled = false;
+
+                this.lblWorkCount.Text = "0";
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("오류가 발생했습니다", ex);
+            }
+        }
+
+        #region 메서드
+
+        /// <summary>
+        /// 처리할 작업입니다.
+        /// </summary>
+        private void DoWork(BackgroundWorker worker, DoWorkEventArgs e)
+        {
+            /*
+             * 실행중인 Form 과 다른 쓰레드에서 동작하므로 
+             * 처리할 메서드에서는 UI 객체의 속성값(Value, Text 등..)을 사용하지 못합니다.
+             * 
+             * 작업에 필요한 값은 매개변수로 전달받아야 하고 UI객체의 상태를 변화시킬 필요가 있는 경우
+             * ProgressChanged
+             * RunWorkerCompleted
+             * 이벤트를 사용해야 합니다.
+             */
+
+            double nMax = 100.0;
+            int nExe = 0;
+
+            while (nMax > nExe)
+            {
+                // 작업이 취소 요청이 되었는지 검사
+                if (worker.CancellationPending)
+                {
+                    e.Cancel = true;
+                    break;
+                }
+                else
+                {
+                    if (ErrorFlag)
+                    {
+                        ErrorFlag = false;
+                        throw new Exception("예외 발생 시뮬레이션");
+                    }
+
+                    // 시간이 걸리는 작업을 실행합니다.
+                    System.Threading.Thread.Sleep(200);
+                    nExe++;
+
+                    // 진행률 업데이트하기 위해 ProgressChanged 이벤트를 발생시킵니다.
+                    worker.ReportProgress((int)((nExe / nMax) * 100));
+                }
+            }
+        }
+
+        private void AppendMessage(string message)
+        {
+            textBox1.AppendText($"[{DateTime.Now:yyyy-MM-dd HH:mm:ss}]: {message}{Environment.NewLine}");
+            textBox1.ScrollToCaret();
+        }
+
+        #endregion
+
+        #region 이벤트 핸들러
+
+        /// <summary>
+        /// 시작버튼 클릭
+        /// </summary>
+        private void Click_btnStart(object sender, EventArgs e)
+        {
+            try
+            {
+                this.progressBar.Value = 0;
+                this.progressBar.Maximum = 100;
+                this.progressBar.Step = 1;
+
+                this.lblWorkCount.Text = "0";
+
+                this.btnStart.Enabled = false;
+                this.btnCancel.Enabled = true;
+
+                this.Cursor = Cursors.WaitCursor;
+
+                AppendMessage("작업을 시작합니다.");
+
+                // 비동기로 작업을 시작합니다. DoWork 이벤트를 발생
+                this.bgWorker.RunWorkerAsync(null);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("오류가 발생했습니다", ex);
+            }
+        }
+
+        /// <summary>
+        /// 취소 버튼 클릭
+        /// </summary>
+        private void Click_btnCancel(object sender, EventArgs e)
+        {
+            try
+            {
+                // 작업 취소요청
+                this.bgWorker.CancelAsync();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("오류가 발생했습니다", ex);
+            }
+        }
+
+        /// <summary>
+        /// 예외 버튼 클릭
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Click_btnError(object sender, EventArgs e)
+        {
+            ErrorFlag = true;
+        }
+
+        /// <summary>
+        /// 작업을 시작
+        /// </summary>
+        private void bgWorker_DoWork(object sender, DoWorkEventArgs e)
+        {
+            this.DoWork((BackgroundWorker)sender, e);
+            e.Result = null;
+
+            //try
+            //{
+            //    this.DoWork((BackgroundWorker)sender, e);
+            //    e.Result = null;
+            //}
+            //catch (Exception ex)
+            //{
+            //    throw new Exception("오류가 발생했습니다", ex);
+            //}
+        }
+
+        /// <summary>
+        /// 작업 진행률을 업데이트
+        /// </summary>
+        private void bgWorker_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            try
+            {
+                this.progressBar.Value = e.ProgressPercentage;
+                int nTmp = 0;
+                int.TryParse(this.lblWorkCount.Text, out nTmp);
+                nTmp++;
+                this.lblWorkCount.Text = nTmp.ToString();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("오류가 발생했습니다", ex);
+            }
+        }
+
+        /// <summary>
+        /// 작업이 완료상태(예외, 취소, 완료)가 되면 발생하는 이벤트입니다
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void bgWorker_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            try
+            {
+                if (e.Error != null)    // 예외 발생
+                {
+                    AppendMessage(String.Format("{0} ==> {1}", "예외가 발생했습니다.", e.Error.Message));
+                }
+                else if (e.Cancelled)   // 작업취소
+                {
+                    AppendMessage("작업이 취소되었습니다.");
+                }
+                else                    // 완료
+                {
+                    AppendMessage("작업이 완료되었습니다.");
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("오류가 발생했습니다", ex);
+            }
+            finally
+            {
+                this.btnStart.Enabled = true;
+                this.btnCancel.Enabled = false;
+                this.Cursor = Cursors.Default;
+            }
+        }
+
+        #endregion
+
+        private static bool ErrorFlag = false;
+    }
+}
